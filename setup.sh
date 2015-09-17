1cd1f8ba61a5b3a0be01bb7a4b9fd4c3d09bb230
arvindkandhare <arvind.kandhare@emc.com>
2015-09-17 16:16:59 +0530

    Checking in latest changes before merge ..

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 cd /host/files
-python object_prep.py --ECSNodes=10.244.0.6,10.244.1.5,10.244.2.6 --Namespace=ns1 --ObjectVArray=ova1 --ObjectVPool=ovp1 --UserName=emccode --DataStoreName=ds1 --VDCName=vdc1 --MethodName=
+python object_prep.py --ECSNodes=10.244.1.6,10.244.0.5,10.244.2.6 --Namespace=ns1 --ObjectVArray=ova1 --ObjectVPool=ovp1 --UserName=emccode --DataStoreName=ds1 --VDCName=vdc1 --MethodName=
